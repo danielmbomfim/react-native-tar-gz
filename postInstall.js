a7a8f92bb78a91d26f5d3a4c05662cbd690f5302
Daniel Bomfim <daniellsmv@hotmail.com>
2024-04-23 14:38:31 -0300

    chore: replaces postinstall command by a js script

---
+++ postInstall.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs');
+
+const filePath = './targz/.cargo/config.toml';
+const ndkHome = process.env.NDK_HOME;
+
+function expandEnvVars() {
+  if (!ndkHome) {
+    throw Error('NDK_HOME environment variable is not defined correctly.');
+  }
+
+  let path = '';
+
+  if (ndkHome.endsWith('/') || ndkHome.endsWith('\\')) {
+    path = ndkHome.substring(0, ndkHome.length - 1).replaceAll('\\', '/');
+  } else {
+    path = ndkHome.replaceAll('\\', '/');
+  }
+
+  const content = fs.readFileSync(filePath, 'utf-8');
+
+  fs.writeFileSync(filePath, content.replaceAll('${NDK_HOME}', path));
+}
+
+expandEnvVars();
