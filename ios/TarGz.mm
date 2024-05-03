2c5eab1630576d11ae694853f3b3651a0efb83ad
Daniel Bomfim <daniellsmv@hotmail.com>
2024-05-02 21:17:28 -0300

    fix: fixes syntax erros on the ios implementation

--- ios/TarGz.mm
+++ ios/TarGz.mm
@@ -1,3 +1,4 @@
+using namespace std;
 #import "TarGz.h"
 
 @implementation TarGz
@@ -5,17 +6,28 @@ RCT_EXPORT_MODULE()
 
 // Don't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
-- compress:(NSString*)source destination:(NSString*) destination resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+- (void)compress:(NSString*)source destination:(NSString*) destination resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
     const char *from = [source UTF8String];
     const char *to = [destination UTF8String];
     
-    double *result = compress_rs(from, to);
+    int8_t result = compress_rs(from, to);
 
     if (result == 0) {
-                const *char error_message = nativeGetError();
-        NSString *parsed_error_message = [NSString stringWithUTF8String:cStrierror_messageng];
+        // int error_length = last_error_length();
+        // NSString *error_message = @"";
+
+        // if (error_length != 0) {
+        //     std::string msg(error_length, '\0');
+        //     int ret = last_error_message(&msg[0], msg.length());
+
+        //     if (ret > 0) {
+        //         error_message = [NSString stringWithUTF8String:ret];
+        //     }
+        // }
+
+        NSError *error = [NSError errorWithDomain:@"error" code:200 userInfo:@{@"Error reason": @"Invalid Input"}];
         
-        reject(parsed_error_message);
+        reject(@"tarball_error", @"Failed to compress", error);
         return;
     }
 
@@ -23,18 +35,28 @@ RCT_EXPORT_MODULE()
     return;
 }
 
-- uncompress:(NSString*) source destination:(NSString*) destination resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject {
+- (void)uncompress:(NSString*) source destination:(NSString*) destination resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
     const char *from = [source UTF8String];
     const char *to = [destination UTF8String];
 
-    double *result = uncompress_rs(from, to);
+    int8_t result = uncompress_rs(from, to);
 
     if (result == 0) {
-        const *char error_message = nativeGetError();
-        NSString *parsed_error_message = [NSString stringWithUTF8String:cStrierror_messageng];
+        // int error_length = last_error_length();
+        // NSString *error_message = @"";
+
+        // if (error_length != 0) {
+        //     std::string msg(error_length, '\0');
+        //     int ret = last_error_message(&msg[0], msg.length());
+
+        //     if (ret > 0) {
+        //         error_message = [NSString stringWithUTF8String:ret];
+        //     }
+        // }
+
+        NSError *error = [NSError errorWithDomain:@"error" code:200 userInfo:@{@"Error reason": @"Invalid Input"}];
         
-        reject(parsed_error_message);
+        reject(@"tarball_error", @"Failed to uncompress", error);
         return;
     }
 
